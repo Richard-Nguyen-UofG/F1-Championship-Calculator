aa11e9c42ff9cce1b187c21b92052da6367c52c8
Richard-Nguyen-UofG <richard_nguyen@live.ca>
2022-04-03 20:06:23 -0400

    Significant improvements to changability and includes fastest lap calculations

--- championshipPermutationsCPP.cpp
+++ championshipPermutationsCPP.cpp
@@ -1,32 +1,63 @@
 #include <iostream>
 #include <iomanip>
 #include <math.h>
+#include <string.h>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 // https://www.geeksforgeeks.org/multithreading-in-cpp/
+// auto filter out anyone out of championship contention
 
 int SCORE_SYSTEM[] = {25, 18, 15, 12, 10, 8, 6, 4, 2, 1, 0};
 const int NUM_PLACES = sizeof(SCORE_SYSTEM) / sizeof(SCORE_SYSTEM[0]);
 
-const int DRIVERX_POINTS[] = {134, 120, 79};
-const int NUM_DRIVERS = sizeof(DRIVERX_POINTS) / sizeof(DRIVERX_POINTS[0]);
+const int NUM_DRIVERS = 3;
+const int NUM_DIVS = 10;
 
-int factorial(int num);
+const int DRIVERX_POINTS[NUM_DIVS][NUM_DRIVERS] =   {
+                                                        {134, 120,  79}, {110,  92,  82}, {150,  80,  78}, {118,  90,  85}, {106, 100,  84},
+                                                        {118, 107,  94}, {172, 162, 117}, {141, 105,  81}, {129, 125,  88}, {  0,   0,   0}
+                                                    };
+
+long long int factorial(int num);
 int nPr(int n, int r);
 
 
 int perms_count = 0;
-int one_race_perms_count = nPr(NUM_PLACES, NUM_DRIVERS);
+int one_race_perms_count = nPr(NUM_PLACES, NUM_DRIVERS) * NUM_PLACES;
 int** one_race_perms;
 
 
+int giveFastestLap(int points) {
+    if (points > 0) {
+        return points + 1;
+    } else {
+        return points;
+    }
+}
+
 void recordPermutation(int a[], int n) {
 
-    for (int i=0; i<n; i++) {
-        one_race_perms[perms_count][i] = a[i];
+    int fastestLap = 0;
+
+    // calculate NUM_PLACES for the same set (for a fastest lap in each place)
+    for (int j = 0; j < NUM_PLACES; j++) {
+
+        // add one permutation of race results
+        for (int i=0; i<n; i++) {
+
+            if (j < NUM_DRIVERS && i == j && a[i] > 0) {
+                fastestLap = 1;
+            } else {
+                fastestLap = 0;
+            }
+
+            one_race_perms[perms_count][i] = a[i] + fastestLap;
+        }
+        perms_count += 1;
     }
-    perms_count += 1;
 }
 
 // Generating permutation using Heap Algorithm
@@ -65,6 +96,11 @@ void kCombination(int a[], int n, int k) {
 }
 
 int main() {
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::duration;
+    using std::chrono::milliseconds;
+
 
     // initialize one_race_perms array
     one_race_perms = new int*[one_race_perms_count];
@@ -72,71 +108,82 @@ int main() {
         one_race_perms[i] = new int[NUM_DRIVERS];
     }
 
+    cout << "There are " << one_race_perms_count << " permutations of one race (with the given inputs)" << endl;
+
+    // no need to recalculate one_race_perms if the number of drivers is constant
+    perms_count = 0;
     kCombination(SCORE_SYSTEM, NUM_PLACES, NUM_DRIVERS);
 
-    unsigned long long int total = 0;
-    unsigned long long int driver_wins[] = {0, 0, 0};
-    unsigned long long int draws = 0;
+    for (int z = 0; z < NUM_DIVS; z++) {
 
-    for (int i = 0; i < one_race_perms_count; i++) {
-        for (int j = 0; j < one_race_perms_count; j++) {
-            for (int k = 0; k < one_race_perms_count; k++) {
+        unsigned long long int total = 0;
+        unsigned long long int driver_wins[NUM_DRIVERS] = {0};
+        unsigned long long int draws = 0;
 
-                for (int l = 0; l < NUM_PLACES; l++) {
-                    int driver_points[] = {0, 0, 0};
+        auto start = high_resolution_clock::now();
 
-                    for(int a = 0; a < NUM_DRIVERS; a++) {
-                        int points = DRIVERX_POINTS[a] + one_race_perms[i][a] + one_race_perms[j][a] + one_race_perms[k][a];
+        for (int i = 0; i < one_race_perms_count; i++) {
+            for (int j = 0; j < one_race_perms_count; j++) {
 
-                        if (l > 10 or l == a) {
-                            points += 1;
-                        }
+                int driver_points[NUM_DRIVERS] = {0};
 
-                        driver_points[a] = points;
-                    }
+                for(int a = 0; a < NUM_DRIVERS; a++) {
+                    int points = DRIVERX_POINTS[z][a] + one_race_perms[i][a] + one_race_perms[j][a];
 
-                    // find max
-                    int max = driver_points[0];
-                    int max_index = 0;
-                    for (int a = 1; a < NUM_DRIVERS; a++) {
-                        if (driver_points[a] > max) {
-                            max = driver_points[a];
-                            max_index = a;
-                        }
-                    }
 
-                    //count max
-                    int max_count = 0;
-                    for (int a = 0; a < NUM_DRIVERS; a++) {
-                        if (driver_points[a] == max) {
-                            max_count += 1;
-                        }
-                    }
-
-                    if (max_count == 1) {
-                        driver_wins[max_index] += 1;
-                    } else {
-                        draws += 1;
-                    }
-                    total += 1;
+                    driver_points[a] = points;
                 }
+
+                // find max
+                int max = driver_points[0];
+                int max_index = 0;
+                for (int a = 1; a < NUM_DRIVERS; a++) {
+                    if (driver_points[a] > max) {
+                        max = driver_points[a];
+                        max_index = a;
+                    }
+                }
+
+                //count max
+                int max_count = 0;
+                for (int a = 0; a < NUM_DRIVERS; a++) {
+                    if (driver_points[a] == max) {
+                        max_count += 1;
+                    }
+                }
+
+                if (max_count == 1) {
+                    driver_wins[max_index] += 1;
+                } else {
+                    draws += 1;
+                }
+                total += 1;
             }
         }
-    }
 
-    for (int i = 0; i < NUM_DRIVERS; i++) {
-        cout << driver_wins[i] << '/' << total << '(' << setprecision(4) << ((float) driver_wins[i]/ (float) total) * 100.0 << "%)\n";
+        auto stop = high_resolution_clock::now();
+
+        cout << "------------------------" << z+1 << "------------------------" << endl;
+        for (int i = 0; i < NUM_DRIVERS; i++) {
+            cout << driver_wins[i] << '/' << total << '(' << setprecision(4) << ((float) driver_wins[i]/ (float) total) * 100.0 << "%)\n";
+        }
+        cout << draws << '/' << total << '(' << setprecision(4) << ((float) draws/ (float) total) * 100.0 << "%)\n";
+        cout << total << '/' << total << '(' << setprecision(4) << ((float) total/ (float) total) * 100.0 << "%)\n";
+
+        auto duration = duration_cast<microseconds>(stop - start);
+        double durationInSeconds = duration.count()/1000000.0; 
+        cout << "completed in " << durationInSeconds << setprecision(7) << " seconds" << endl;
+
+        cout << "~" << int(total/durationInSeconds) << " permutations/s" << endl;
     }
-    cout << draws << '/' << total << '(' << setprecision(4) << ((float) draws/ (float) total) * 100.0 << "%)\n";
-    cout << total << '/' << total << '(' << setprecision(4) << ((float) total/ (float) total) * 100.0 << "%)\n";
 
     return 0;
 }
 
 
 
-int factorial(int num) {
-    int fact = 1;
+long long int factorial(int num) {
+    long long int fact = 1;
     for (int i = 1; i < num+1; i++) {
         fact = fact * i;
     }
